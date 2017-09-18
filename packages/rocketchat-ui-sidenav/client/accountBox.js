53eca62f3eb93db5afc961a013dfec303175dcbc
NexFive <anatolygorban3@gmail.com>
2017-09-18 06:53:42 +0300

    first commit

---
+++ packages/rocketchat-ui-sidenav/client/accountBox.js
@@ -0,0 +1,131 @@
+/* globals popover */
+
+Template.accountBox.helpers({
+	myUserInfo() {
+		if (Meteor.user() == null && RocketChat.settings.get('Accounts_AllowAnonymousRead')) {
+			return {
+				name: t('Anonymous'),
+				fname: t('Anonymous'),
+				status: 'online',
+				visualStatus: t('online'),
+				bullet: 'general-success-background',
+				username: 'anonymous'
+			};
+		}
+
+		const user = Meteor.user() || {};
+		const { name, username } = user;
+		const userStatus = Session.get(`user_${ username }_status`);
+
+		return {
+			name: Session.get(`user_${ username }_name`) || username,
+			status: Session.get(`user_${ username }_status`),
+			visualStatus: t(userStatus.charAt(0).toUpperCase() + userStatus.slice(1)),
+			bullet: userStatus,
+			_id: Meteor.userId(),
+			username,
+			fname: name || username
+		};
+	},
+
+	isAnonymous() {
+		if (Meteor.userId() == null && RocketChat.settings.get('Accounts_AllowAnonymousRead')) {
+			return 'disabled';
+		}
+	}
+});
+
+Template.accountBox.events({
+	'click .sidebar__account.active'() {
+		let adminOption;
+		if (RocketChat.authz.hasAtLeastOnePermission(['view-statistics', 'view-room-administration', 'view-user-administration', 'view-privileged-setting' ]) || (RocketChat.AdminBox.getOptions().length > 0)) {
+			adminOption = {
+				icon: 'customize',
+				name: t('Administration'),
+				type: 'open',
+				id: 'administration'
+			};
+		}
+
+		const accountBox = document.querySelector('.sidebar__account');
+
+		const config = {
+			popoverClass: 'account',
+			columns: [
+				{
+					groups: [
+						{
+							title: t('User'),
+							items: [
+								{
+									icon: 'circle',
+									name: t('Online'),
+									type: 'set-state',
+									id: 'online',
+									class: 'online'
+								},
+								{
+									icon: 'circle',
+									name: t('Away'),
+									type: 'set-state',
+									id: 'away',
+									class: 'away'
+								},
+								{
+									icon: 'circle',
+									name: t('Busy'),
+									type: 'set-state',
+									id: 'busy',
+									class: 'busy'
+								},
+								{
+									icon: 'circle',
+									name: t('Invisible'),
+									type: 'set-state',
+									id: 'offline',
+									class: 'offline'
+								}
+							]
+						},
+						{
+							items: AccountBox.getItems().map(item => {
+								return {
+									icon: item.icon,
+									name: t(item.name),
+									type: 'open',
+									id: item.name
+								};
+							}).concat([
+								adminOption,
+								{
+									icon: 'user',
+									name: t('My_Account'),
+									type: 'open',
+									id: 'account'
+								},
+								{
+									icon: 'sign-out',
+									name: t('Logout'),
+									type: 'open',
+									id: 'logout'
+								}
+							])
+						}
+
+					]
+				}
+			],
+			position: {
+				top: accountBox.offsetHeight + parseInt(getComputedStyle(accountBox)['padding-bottom'].replace('px', '')),
+				left: parseInt(getComputedStyle(accountBox)['padding-left'].replace('px', ''))
+			},
+			customCSSProperties: {
+				width: `${ accountBox.offsetWidth - parseInt(getComputedStyle(accountBox)['padding-left'].replace('px', '')) * 2 }px`
+			}
+		};
+
+		popover.open(config);
+	}
+});
+
+Template.accountBox.onRendered(() => AccountBox.init());
